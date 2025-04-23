9782c9067c4b568db8064bc9fb84662cc3eb1a53
kotjuz <rkotarski10@gmail.com>
2025-04-23 19:00:23 +0200

    added new properties check

--- data_manager.py
+++ data_manager.py
@@ -1,7 +1,14 @@
 class DataManager():
     def __init__(self, database):
         self.database=database
-        self.interesting_brands=["peugeot","mercedes","volkswagen", "citroen", "toyota", "kia", "bmw", "audi", "seat", "opel", "skoda", "volvo", "renault", "dacia"]
+        self.interesting_properties = {
+            "brands": ["peugeot","mercedes","volkswagen", "citroen", "toyota", "kia", "bmw", "audi", "seat", "opel", "skoda", "volvo", "renault", "dacia"],
+            "min_price": 100,
+            "max_price": 30000,
+            "min_year": 2000,
+            "max_year": 2026,
+            "max_mileage": 300000
+        }
 
     def check_for_new_cars(self, current_page_list):
         new_cars=[]
@@ -21,9 +28,20 @@ class DataManager():
     def check_for_interesting_cars(self, new_cars):
         interesting_cars = []
         for car in new_cars:
+            matches = 0
             brand = car["name"].split()[0]
-            if brand.lower() in self.interesting_brands:
+            if brand.lower() in self.interesting_properties["brands"]:
+                matches += 1
+            if self.interesting_properties["min_price"] <= car['price'] <= self.interesting_properties["max_price"]:
+                matches += 1
+            if self.interesting_properties["min_year"] <= car['year'] <= self.interesting_properties["max_year"]:
+                matches += 1
+            if car['mileage'] <= self.interesting_properties["max_mileage"]:
+                matches += 1
+
+            if matches == 4:
                 interesting_cars.append(car)
+                
         return interesting_cars
 
 
